77b338d00d3bdcb92ebc844f55a78790053d9ad2
Mark Muller <mark.muller.w@gmail.com>
2020-03-04 12:09:34 +0800

    create booking user validation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
     @collections = current_user.collections
     @made_bookings = current_user.bookings
     @received_bookings = Booking.all.select { |booking| booking.collection.user == current_user }
+    # raise
   end
 
   def account
