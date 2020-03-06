16adb84e1b52d1d1a1ffc494ed0128bd40db683e
Vincent Rehms <v.rehms@gmail.com>
2020-03-06 16:52:35 +0800

    styled the collection show page

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -17,13 +17,13 @@ class CollectionsController < ApplicationController
       }
     end
     @collections = policy_scope(Collection)
-    
-    if params[:q]
-      @collections = Collection.where("title ILIKE ?", "%" + params[:q] + "%")
-    end
+
+    # if params[:q]
+    #   @collections = Collection.where("title ILIKE ?", "%" + params[:q] + "%")
+    # end
     # raise
   end
-end
+
 
 
 
